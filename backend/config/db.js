50ebf26efe261a3ba218030ecbab6aaaf9a2e14c
muhammed anshad k <mohammedanshad20@gmail.com>
2023-11-30 11:22:58 +0530

    sixth commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectDB = async() => {
-
-    try{
-        await mongoose.connect('mongodb://localhost/m3care',{ useNewUrlParser: true, useUnifiedTopology: true});
-        console.log("successfully connected")
-    }catch(error){
-      console.log(error)
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/m3care', { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Successfully connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
     }
-    
 }
 
-module.exports = connectDB; 
-
-
+module.exports = connectDB;
